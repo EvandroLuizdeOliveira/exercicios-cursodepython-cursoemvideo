818538047d5c643b017bd07d5941bf816fe2c9c0
Evandro Luiz de Oliveira <evandro.lu.oli@gmail.com>
2022-07-01 16:50:03 -0300

    exercises class sixteen pt2
    some modification on the exercises

--- exercicios-python/70-79/ex075.py
+++ exercicios-python/70-79/ex075.py
@@ -1,13 +1,13 @@
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro número: '))
-num3 = int(input('Digite outro número: '))
-num4 = int(input('Digite outro número: '))
+num3 = int(input('Digite mais um número: '))
+num4 = int(input('Digite o último número: '))
 numeros = (num1, num2, num3, num4)
 if 3 in numeros:
     resultado = f'O número 3 foi digitado primeiro na posição {(numeros.index(3))+1}'
 else:
     resultado = f'O número 3 não foi digitado'
-print(f'\nO número 9 apareceu {numeros.count(9)} vezes\n{resultado}\nOs valores pares digitados foram', end=' ')
+print(f'\nVocê digitou os valores {numeros}\nO número 9 apareceu {numeros.count(9)} vezes\n{resultado}\nOs valores pares digitados foram', end=' ')
 for par in range(0, len(numeros)):
     if par % 2 == 0:
         print(numeros[par], end=' ')
