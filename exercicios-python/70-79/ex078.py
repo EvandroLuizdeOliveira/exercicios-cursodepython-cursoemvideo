9efc6cda79ee9151f165cfd01a54ca33670ae8dc
Evandro Luiz de Oliveira <evandro.lu.oli@gmail.com>
2022-07-18 17:11:26 -0300

    update class 17 exercises

--- exercicios-python/70-79/ex078.py
+++ exercicios-python/70-79/ex078.py
@@ -3,7 +3,14 @@ O programa vai ler 5 números e guardar em uma lista. Depois vai mostrar qual é
 """
 valores = []
 for count in range(0, 5):
-    valores.append(int(input('Digite um número: ')))
-print(f'Você digitou os valores {valores}'
-f'\nO maior valor digitado foi {max(valores)} nas posições {valores.index(max(valores))}'
-f'\nO menor valor digitado foi {min(valores)} nas posições {valores.index(min(valores))}')
+    valores.append(int(input(f'Digite um valor para a posição {count}: ')))
+print(f'{"=-=" * 20}'
+      f'\nVocê digitou os valores {valores}'
+      f'\nO maior valor digitado foi {max(valores)} nas posições', end=' ')
+for c, v in enumerate(valores):
+        if v == max(valores):
+            print(f'{c}', end='... ')
+print(f'\nO menor valor digitado foi {min(valores)} nas posições', end=' ')
+for c, v in enumerate(valores):
+        if v == min(valores):
+            print(f'{c}', end='... ')
--- exercicios-python/80-89/ex083.py
+++ exercicios-python/80-89/ex083.py
@@ -1,8 +1,9 @@
 """
-Um programa que lê uma expressão matemática que use parênteses digitada pelo usuário  e analisa se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+Um programa que lê uma expressão matemática que use parênteses digitada pelo usuário e analisa se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 """
 expressao = str(input('Digite uma expressão: ')).strip()
-dividido = expressao.split('(')
-print(dividido)
-print(expressao.count(')'))
-print(len(dividido))
+expressao = list(expressao)
+if expressao.count('(') == expressao.count(')'):
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão está errada!')
