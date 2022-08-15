6f2b3e1dbe0e3d194564482eba947c8a7249d72d
Evandro Luiz de Oliveira <evandro.lu.oli@gmail.com>
2022-08-15 16:47:34 -0300

    update exercises

--- exercicios-python/80-89/ex084.py
+++ exercicios-python/80-89/ex084.py
@@ -3,14 +3,27 @@ Programa lê nome e peso de várias pessoas e guarda tudo em uma lista. No final
 """
 pessoas = []
 dados = []
-contador = 0
+maiores = []
+menores = []
+contador = maior = menor = 0
 while True:
     dados.append(str(input('Digite o nome: ')).strip())
     dados.append(int(input('Digite o peso: ')))
     pessoas.append(dados[:])
+    if contador == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+            maiores.append(dados[0])
+        if dados[1] < menor:
+            menor = dados[1]
+            menores.append(dados[0])
     dados.clear()
     contador += 1
     continuar = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
     if continuar == 'N':
         break
-print(f'{contador} pessoas foram cadastradas!')
+print(f'{contador} pessoas foram cadastradas!'
+      f'\n{maior} {maiores}'
+      f'\n{menor} {menores}')
