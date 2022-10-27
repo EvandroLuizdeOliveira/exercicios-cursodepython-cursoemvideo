56ad7eb014cb6cd1bc53777dc69decca5d6338f9
Evandro Luiz de Oliveira <evandro.lu.oli@gmail.com>
2022-10-27 17:41:16 -0300

    Update ex 84

--- exercicios-python/80-89/ex084.py
+++ exercicios-python/80-89/ex084.py
@@ -1,29 +1,27 @@
 """
 Programa lê nome e peso de várias pessoas e guarda tudo em uma lista. No final mostra quantas pessoas foram cadastradas, uma listagem com as pessoas mais pesadas e uma listagem com as pessoas mais leves.
 """
-pessoas = []
+temporario = []
 dados = []
-maiores = []
-menores = []
-contador = maior = menor = 0
+oas_leves = []
 while True:
-    dados.append(str(input('Digite o nome: ')).strip())
-    dados.append(int(input('Digite o peso: ')))
-    pessoas.append(dados[:])
-    if contador == 0:
-        maior = menor = dados[1]
-    else:
-        if dados[1] > maior:
-            maior = dados[1]
-            maiores.append(dados[0])
-        if dados[1] < menor:
-            menor = dados[1]
-            menores.append(dados[0])
-    dados.clear()
-    contador += 1
-    continuar = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
+    temporario.append(str(input('Digite o nome: ')).strip())
+    temporario.append(float(input('Digite o peso: ')))
+    dados.append(temporario[:])
+    temporario.clear()
+    while True:
+        continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if continuar == "S" or continuar == "N":
+            break
     if continuar == 'N':
         break
-print(f'{contador} pessoas foram cadastradas!'
-      f'\n{maior} {maiores}'
-      f'\n{menor} {menores}')
+print(f'{"=-=" * 20}'
+      f'\n{len(dados)} pessoas foram cadastradas!')
+print(f'O maior peso foi de {max(dados)[1]}Kg. Peso de ', end='')
+for posicao in dados:
+    if posicao[1] == max(dados)[1]:
+        print(f'[{posicao[0]}]')
+print(f'O menor peso foi de {min(dados)[1]}Kg. Peso de ', end='')
+for posicao in dados:
+    if posicao[1] == min(dados)[1]:
+        print(f'[{posicao[0]}]')
